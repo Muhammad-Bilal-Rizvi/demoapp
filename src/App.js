fed8bfad0783f9e90a2a241e3ea60bd4746d3074
Muhammad Bilal Rizvi <73776975+Muhammad-Bilal-Rizvi@users.noreply.github.com>
2020-11-13 23:07:46 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Dinner from './dinner.js';
+
+//  import logo from './logo.svg';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dinner dishName="Nahari" sweetDish="Kheer"/>
+
+      <hr/>
+
+      <Dinner dishName="Biryani" sweetDish="Jaleebi"/>
+
+      <hr/>
+      
+      <Dinner dishName="Karahi" sweetDish="Gajar Ka Halwa"/>
+
+
     </div>
+
   );
 }
 
